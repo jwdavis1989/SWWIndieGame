b23d62a85027a959ce7dfe07a0242cec0c6eebea
Alec Straw <47575949+akstraw@users.noreply.github.com>
2024-12-04 20:13:49 -0600

    some aggro code, make drops smaller, exp position

--- Assets/Scripts/Entities/Monster/EnemyStatsManager.cs
+++ Assets/Scripts/Entities/Monster/EnemyStatsManager.cs
@@ -4,18 +4,21 @@ using UnityEngine;
 
 public class EnemyStatsManager : CharacterStatsManager
 {
+    [Header("Amount of gold dropped")]
     public int goldDrop = 0;
+    [Header("Gold drop chance as 0-1")]
     public float goldDropChance = 0;// % as 0-1
-    public int expDrop = 0;
-
+    [Header("Amount of expierence points dropped for weapon")]
+    public int expDropAmt = 0;
+    [Header("Tinker component drop chance as 0-1")]
     public float[] componentDropChances = new float[(int)TinkerComponentType.Weapon]; //drop chance of all components (exluding weapon components)
     public KeyValuePair<TinkerComponentType, float>[] componentDropChances3 = new KeyValuePair<TinkerComponentType, float>[(int)TinkerComponentType.Weapon];
     //public Dictionary<TinkerComponentType, float> componentDropChances = new Dictionary<TinkerComponentType, float>();
-    public float[] weaponDropChances = new float[(int)WeaponType.UNKNOWN]; //drop chance of all weapons
-    public void DoAllDrops()
+    //public float[] weaponDropChances = new float[(int)WeaponType.UNKNOWN]; //drop chance of all weapons
+    public void DoAllDrops(bool isMainHand)
     {
-        if (expDrop > 0)
-            DropExp();
+        if (expDropAmt > 0)
+            DropExp(isMainHand);
         if (goldDropChance > Random.value)
             DropGold();
         for(int i = 0; i < componentDropChances.Length; i++)
@@ -25,17 +28,13 @@ public class EnemyStatsManager : CharacterStatsManager
                 DropComponent((TinkerComponentType)i);
             }
         }
-        for (int i = 0; i < weaponDropChances.Length; i++)
-        {
-            if (weaponDropChances[i] > Random.value)
-            {
-                DropWeapon((WeaponType)i);
-            }
-        }
-    }
-    public float GetWeaponDropChance(WeaponType type)
-    {
-        return weaponDropChances[(int)type];
+        //for (int i = 0; i < weaponDropChances.Length; i++)
+        //{
+        //    if (weaponDropChances[i] > Random.value)
+        //    {
+        //        DropWeapon((WeaponType)i);
+        //    }
+        //}
     }
     public float GetComponentDropChance(TinkerComponentType type)
     {
@@ -45,17 +44,15 @@ public class EnemyStatsManager : CharacterStatsManager
     {
         ItemDropContainer.instance.DropGold(transform, goldDrop);
     }
-    public void DropExp()
+    public void DropExp(bool isMainHand)
     {
-        ItemDropContainer.instance.DropExp(transform, expDrop);
-    }
-    /** 
-     *  Drops a weapon at this objects location 
-     *  Returns a reference to the dropped weapon
-     */
-    public GameObject DropWeapon(WeaponType type)
-    {
-        return Instantiate(ItemDropContainer.instance.DropWeapon(type, transform));
+        Transform t = transform;
+        Vector3 p = t.position; 
+        float newX = p.x + (Random.value * (Random.value > 0.5? -1:1));
+        float newY = p.y + (Random.value * (Random.value > 0.5 ? -1 : 1));
+        float newZ = p.z + (Random.value * (Random.value > 0.5 ? -1 : 1));
+        t.position = new Vector3 (newX, newY, newZ);
+        ItemDropContainer.instance.DropExp(t, expDropAmt, isMainHand);
     }
     /** 
      *  Drops a TinkerComponent at this objects location 
@@ -65,4 +62,16 @@ public class EnemyStatsManager : CharacterStatsManager
     {
         return Instantiate(ItemDropContainer.instance.DropComponent(type, transform));
     }
+    /** 
+     *  Drops a weapon at this objects location 
+     *  Returns a reference to the dropped weapon
+     */
+    //public GameObject DropWeapon(WeaponType type)
+    //{
+    //    return Instantiate(ItemDropContainer.instance.DropWeapon(type, transform));
+    //}
+    //public float GetWeaponDropChance(WeaponType type)
+    //{
+    //    return weaponDropChances[(int)type];
+    //}
 }
