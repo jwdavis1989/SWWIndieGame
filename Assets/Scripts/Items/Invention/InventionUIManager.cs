38f2f50161ae0b12a1079b42e1ca34ec751cb9fb
Alec Straw <47575949+akstraw@users.noreply.github.com>
2025-05-26 16:58:03 -0500

    add/remove idea from list

--- Assets/Scripts/Items/Invention/InventionUIManager.cs
+++ Assets/Scripts/Items/Invention/InventionUIManager.cs
@@ -48,11 +48,11 @@ public class InventionUIManager : MonoBehaviour
     }
 
     /**
-     * Clear component list and reload it with current values
+     * Clear idea list and reload it with current values
      */
-    private int currentIdeasPage = 0;
     [Header("Total ideas able to display per row when selecting an idea")]
     public int ideasPerRow = 6;
+    private int currentIdeasPage = 0;
     void LoadIdeasToScreen()
     {
         foreach (Transform child in ownedIdeasGrid.transform)
@@ -92,29 +92,36 @@ public class InventionUIManager : MonoBehaviour
                 Debug.Log("ADDING BEHAVIOUR TO  BUTTON");//astest
                 gridScript.mainButton.onClick.AddListener(() => {
                     Debug.Log("IN BEHAVIOUR "+ activeIdea);//astest
+                    int oldActiveIdea = activeIdea;
+                    GridElementController usedIdeaPanel;
                     if (activeIdea == 1)
                     {
                         activeIdea++;
-                        firstIdea.SetActive(true);
-                        firstIdea.GetComponent<GridElementController>().mainButtonForeground.GetComponent<RawImage>().texture = gridScript.mainButtonForeground.GetComponent<RawImage>().texture;
-                        firstIdea.GetComponent<GridElementController>().bottomText.text = "" + (IdeaType)ideaIndex;
+                        usedIdeaPanel = firstIdea.GetComponent<GridElementController>();
                     }
                     else if (activeIdea == 2)
                     {
                         activeIdea++;
-                        secondIdea.SetActive(true);
-                        secondIdea.GetComponent<GridElementController>().mainButtonForeground.GetComponent<RawImage>().texture = gridScript.mainButtonForeground.GetComponent<RawImage>().texture;
-                        secondIdea.GetComponent<GridElementController>().bottomText.text = "" + (IdeaType)ideaIndex;
+                        usedIdeaPanel = secondIdea.GetComponent<GridElementController>();
                     }
                     else
                     {
-                        thirdIdea.SetActive(true);
-                        thirdIdea.GetComponent<GridElementController>().mainButtonForeground.GetComponent<RawImage>().texture = gridScript.mainButtonForeground.GetComponent<RawImage>().texture;
-                        thirdIdea.GetComponent<GridElementController>().bottomText.text = "" + (IdeaType)ideaIndex;
+                        usedIdeaPanel = thirdIdea.GetComponent<GridElementController>();
                     }
+                    usedIdeaPanel.gameObject.SetActive(true);
+                    //set picture
+                    usedIdeaPanel.GetComponent<GridElementController>().mainButtonForeground.GetComponent<RawImage>().texture = gridScript.mainButtonForeground.GetComponent<RawImage>().texture;
+                    //secondIdea.GetComponent<GridElementController>().bottomText.text = "" + (IdeaType)ideaIndex;
+                    usedIdeaPanel.mainButton.onClick.AddListener(() =>{
+                        Debug.Log("Used Idea Clicked");
+                        usedIdeaPanel.gameObject.SetActive(false);
+                        //reenable this idea
+                        gridScript.mainButton.interactable = true;
+                        activeIdea = oldActiveIdea;
+                    });
+                        //disable this idea
+                        gridScript.mainButton.interactable = false;
                 });
-                // cant use component. disable the button
-                //else gridScript.mainButton.interactable = false;
             }
         }
         int numOfPage = totalIdeaCount / ideasPerRow;
