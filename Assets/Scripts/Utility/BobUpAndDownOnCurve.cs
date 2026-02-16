77479361eb909134927f0a285957a35314b09eb8
jwdavis1989 <jwdavis8589@gmail.com>
2026-02-16 02:48:12 -0600

    Refined BobAndDownOnCurve to add the ability to delay bobbing by a flat value or a random value.

--- Assets/Scripts/Utility/BobUpAndDownOnCurve.cs
+++ Assets/Scripts/Utility/BobUpAndDownOnCurve.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 public class BobUpAndDownOnCurve : MonoBehaviour
 {
     public AnimationCurve bobCurve;
+    private bool isBobbing = false;
 
     [Header("Bobbing Parameters")]
     public float bobSpeed = 1f;
     public float heightMultiplier = 1f;
+
+    [Header("Bobbing Delay Parameters")]
+    public bool isBobbingDelayed = false;
+    public float delayBeforeBobbingInSeconds = 0f;
+    public bool isBobbingDelayRandom = false;
+    public float bobbingDelayRandomRange = 0.1f;
 
     private Vector3 originalLocalPosition;
 
@@ -16,14 +23,39 @@ public class BobUpAndDownOnCurve : MonoBehaviour
     void Start()
     {
         originalLocalPosition = transform.localPosition;
+        if (!isBobbingDelayed)
+        {
+            isBobbing = true;
+        }
+        else
+        {
+            float newBobDelay = delayBeforeBobbingInSeconds;
+
+            if (isBobbingDelayRandom)
+            {
+                newBobDelay = Random.Range(0, bobbingDelayRandomRange);
+            }
+            
+            StartCoroutine(BobAfterDelay(newBobDelay));
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        float time = Time.time * bobSpeed;
-        float yOffset = bobCurve.Evaluate(time % bobCurve.length) * heightMultiplier;
+        if (isBobbing)
+        {
+            float time = Time.time * bobSpeed;
+            float yOffset = bobCurve.Evaluate(time % bobCurve.length) * heightMultiplier;
 
-        transform.localPosition = new Vector3(originalLocalPosition.x, originalLocalPosition.y + yOffset, originalLocalPosition.z);
+            transform.localPosition = new Vector3(originalLocalPosition.x, originalLocalPosition.y + yOffset, originalLocalPosition.z);
+        }
+    }
+
+    IEnumerator BobAfterDelay(float delayDurationInSeconds)
+    {
+        yield return new WaitForSeconds(delayDurationInSeconds);
+
+        isBobbing = true;
     }
 }
