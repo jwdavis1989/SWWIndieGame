2b96c318d5ed724a6152ad89ceaeff14fede29ef
Alec Straw <47575949+akstraw@users.noreply.github.com>
2026-03-04 17:41:35 -0600

    some dungeon level stuff

---
+++ Assets/Scripts/Dungeon/UI/DungeonLevelNodeUI.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DungeonLevelNodeUI : MonoBehaviour
+{
+    public Button button;
+    public Image dungeonLevelImage;
+    public Sprite dungeonLevelSprite;
+
+    public bool showHiddenSprite = true;
+    public bool locked = false;
+
+    //TODO - Onhover, onclick, show/hide, lock/unlock
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
