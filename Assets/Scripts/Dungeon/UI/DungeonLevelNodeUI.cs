e13b08873ba3f9ccef279f8c5fe10e3c4a168a98
Alec Straw <47575949+akstraw@users.noreply.github.com>
2026-03-10 20:44:02 -0500

    add branch to example dungeon, disable player during lvl select

--- Assets/Scripts/Dungeon/UI/DungeonLevelNodeUI.cs
+++ Assets/Scripts/Dungeon/UI/DungeonLevelNodeUI.cs
@@ -6,14 +6,18 @@ using UnityEngine.UI;
 
 public class DungeonLevelNodeUI : MonoBehaviour
 {
+    [Header("Case insensitve I.D. for this level")]
     public string dungeonLevelId;
+    [Header("Holds OnClick & button highlight")]
     public Button button;
+
+    [Header("Marks level as the dungeon entrance. (Always unlocked)")]
+    public bool entrance = false;
+
+    //currently unused
     public Image dungeonLevelImage;
     public Sprite dungeonLevelSprite;
-
     public bool showHiddenSprite = true;
-    public bool entrance = false;
-    //public bool completed = false;
 
     public DungeonLevelManager dungeonLevelManager;
 
@@ -34,6 +38,8 @@ public class DungeonLevelNodeUI : MonoBehaviour
     }
     public void DungeonLevelOnClick()
     {
+        PlayerInputManager.instance.SafeEnable();
+        Time.timeScale = 1;
         DungeonManager.EnterDungeonLevel(dungeonLevelManager.dungeonId, dungeonLevelId);//
     }
 }
