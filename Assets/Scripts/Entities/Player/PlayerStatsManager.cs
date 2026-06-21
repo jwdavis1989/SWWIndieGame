976c8d458ff4723591d07d70fd48dc8a5fe972a3
jwdavis1989 <jwdavis8589@gmail.com>
2026-06-21 03:35:41 -0500

    Fixed the Field of View not updating once out of Fuel.

--- Assets/Scripts/Entities/Player/PlayerStatsManager.cs
+++ Assets/Scripts/Entities/Player/PlayerStatsManager.cs
@@ -16,23 +16,26 @@ public class PlayerStatsManager : CharacterStatsManager
     [SerializeField] private int flickerCount = 6;             // How many times it blinks before dying
     [SerializeField] private float minFlickerDelay = 0.05f;    // Shortest time light stays on/off
     [SerializeField] private float maxFlickerDelay = 0.25f;    // Longest time light stays on/off
-    private bool isFlickering = false;  
+    private bool isFlickering = false;
 
-    protected override void Awake() {
+    protected override void Awake()
+    {
         base.Awake();
 
         player = GetComponent<PlayerManager>();
     }
 
-    protected override void Start() {
+    protected override void Start()
+    {
         base.Start();
 
         //Called here in tutorial, but likely obsolete with other changes made in our version
-            // CalculateHealthBasedOnfortitudeLevel(fortitude);
-            // CalculateStaminaBasedOnEnduranceLevel(endurance);
+        // CalculateHealthBasedOnfortitudeLevel(fortitude);
+        // CalculateStaminaBasedOnEnduranceLevel(endurance);
     }
 
-    public override void Update() {
+    public override void Update()
+    {
         base.Update();
 
         HandleCheckFuelTank();
@@ -76,7 +79,7 @@ public class PlayerStatsManager : CharacterStatsManager
 
                 //TODO: Add a coroutine call here to play some beeping and red flashing for polish
             }
-            else if (!player.isRunningOnEmergencyPowerLevels && currentFuel < maxFuel/2)
+            else if (!player.isRunningOnEmergencyPowerLevels && currentFuel < maxFuel / 2)
             {
                 player.isRunningOnEmergencyPowerLevels = true;
 
@@ -86,36 +89,40 @@ public class PlayerStatsManager : CharacterStatsManager
                 }
 
             }
+            else
+            {
+                PlayerInputManager.instance.currentSprintCameraFieldOfViewMaximum = PlayerInputManager.instance.sprintCameraFieldOfViewMaximumWithFuel;
+            }
         }
         else
         {
-            PlayerInputManager.instance.currentSprintCameraFieldOfViewMaximum = PlayerInputManager.instance.sprintCameraFieldOfViewMaximumWithFuel;
+            PlayerInputManager.instance.currentSprintCameraFieldOfViewMaximum = PlayerInputManager.instance.sprintCameraFieldOfViewMaximum;
         }
     }
 
     private IEnumerator FlickerAndShutOffFlashlight()
-{
-    isFlickering = true;
-
-    //Loop through the flicker count to create an erratic blinking pattern
-    for (int i = 0; i < flickerCount; i++)
     {
-        bool toggleState = (i % 2 == 0); // Alternates true/false every iteration
+        isFlickering = true;
 
-        if (player.flashlight != null) player.flashlight.SetActive(toggleState);
-        if (player.cameraflashlight != null) player.cameraflashlight.SetActive(toggleState);
+        //Loop through the flicker count to create an erratic blinking pattern
+        for (int i = 0; i < flickerCount; i++)
+        {
+            bool toggleState = (i % 2 == 0); // Alternates true/false every iteration
 
-        //TODO Add SFX
+            if (player.flashlight != null) player.flashlight.SetActive(toggleState);
+            if (player.cameraflashlight != null) player.cameraflashlight.SetActive(toggleState);
 
-        //Wait a random chunk of time to make the flicker look unstable and broken
-        yield return new UnityEngine.WaitForSeconds(UnityEngine.Random.Range(minFlickerDelay, maxFlickerDelay));
+            //TODO Add SFX
+
+            //Wait a random chunk of time to make the flicker look unstable and broken
+            yield return new UnityEngine.WaitForSeconds(UnityEngine.Random.Range(minFlickerDelay, maxFlickerDelay));
+        }
+
+        //Hard shutoff
+        if (player.flashlight != null) player.flashlight.SetActive(false);
+        if (player.cameraflashlight != null) player.cameraflashlight.SetActive(false);
+
+        isFlickering = false;
     }
-
-    //Hard shutoff
-    if (player.flashlight != null) player.flashlight.SetActive(false);
-    if (player.cameraflashlight != null) player.cameraflashlight.SetActive(false);
-
-    isFlickering = false;
-}
 
 }
